1b80d134f8038ff1ec079f5a337a2d029d0907bd
L Frazer <lfrazer@github.com>
2019-03-11 15:16:38 -0400

    add note about not sorting ctags

--- classstripper.py
+++ classstripper.py
@@ -4,7 +4,8 @@ import os
 import json
 
 #pass ctags-Universal JSON output into this program to parse out class / structs from C++ headers (e.g. cmd>python classstripper.py typeinfo.json)
-#Example cmd line:  ctags --c++-kinds=+p --fields=+ianS --extras=+q --output-format=json /pathto/yourcode/*.h > typeinfo.json
+#Important note:  Tell ctags not to sort to keep member variables in correct order
+#Example cmd line:  ctags --c++-kinds=+p --fields=+ianS --extras=+q --sort=no --output-format=json /pathto/yourcode/*.h > typeinfo.json
 
 classIndex = {}
 memberIndex = {}  # index of lists by class name
